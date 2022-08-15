9b59a4293bc252e725463223ba882f0b13bcefaf
Ryan Huber <rjhuber83@gmail.com>
2022-08-15 01:29:38 -0400

    add nextui and landing page

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+
+const theme = createTheme({
+    type: 'light',
+    theme: {
+        fonts: {
+            sans: 'Fira Sans Condensed',
+        },
+    },
+});
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-    <Component {...pageProps} />
+    <NextUIProvider theme={theme}>
+        <Component {...pageProps} />
+    </NextUIProvider>
 );
 
 export default MyApp;
